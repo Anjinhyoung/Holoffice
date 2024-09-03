97548d3a14d096ab18f43cf42ffdd40bcc09c4cb
githeed <tmdgmldhk11@gmail.com>
2024-09-03 17:46:38 +0900

    11

--- Assets/HSH/Scripts/HttpTest.cs
+++ Assets/HSH/Scripts/HttpTest.cs
@@ -10,7 +10,7 @@ using UnityEngine.UI;
 [System.Serializable]
 public struct TextInfo
 {
-    public string inputText;
+    public string input_text;
 }
 public class HttpTest : MonoBehaviour
 {
@@ -28,7 +28,7 @@ public class HttpTest : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             HttpInfo info = new HttpInfo();
-            info.url = "https://e098-222-103-183-137.ngrok-free.app/redoc";
+            info.url = "https://snake-hopeful-urchin.ngrok-free.app/summarize_meeting";
             info.onComplete = (DownloadHandler downloadHandler) =>
             {
                 print(downloadHandler.text);
@@ -44,13 +44,14 @@ public class HttpTest : MonoBehaviour
         {
             // 가상의 나의 데이터를 만들자
             TextInfo textInfo = new TextInfo();
-            textInfo.inputText = "테스트1111";
+            textInfo.input_text = "동해물과 백두산이 마르고 닳도록 \r\n하느님이 보우하사 우리나라 만세. \r\n무궁화 삼천리 화려강산 \r\n대한 사람, 대한으로 길이 보전하세\r\n\r\n남산 위에 저 소나무, 철갑을 두른 듯 \r\n바람서리 불변함은 우리 기상일세. \r\n무궁화 삼천리 화려강산 \r\n대한 사람, 대한으로 길이 보전하세\r\n\r\n가을 하늘 공활한데 높고 구름 없이 \r\n밝은 달은 우리 가슴 일편단심일세. \r\n무궁화 삼천리 화려강산 \r\n대한 사람, 대한으로 길이 보전하세\r\n\r\n이 기상과 이 맘으로 충성을 다하여 \r\n괴로우나 즐거우나 나라 사랑하세. \r\n무궁화 삼천리 화려강산 \r\n대한 사람, 대한으로 길이 보전하세";
             
 
             HttpInfo info = new HttpInfo();
-            info.url = "https://e098-222-103-183-137.ngrok-free.app/redoc";
+            info.url = "https://snake-hopeful-urchin.ngrok-free.app/summarize_meeting";
             info.body = JsonUtility.ToJson(textInfo);
-            info.contentType = "text";
+            info.contentType = "application/json";
+            //info.contentType = "text";
             info.onComplete = (DownloadHandler downloadHandler) =>
             {
                 print(downloadHandler.text);
