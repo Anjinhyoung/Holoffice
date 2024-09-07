5ccde621c2b922aaaa00199e46ad008a02cb5992
wlsgud2075 <wlsgud2075@naver.com>
2024-09-07 20:08:45 +0900

    UI 세부 수정

--- Assets/Scripts/BlackBoard/Drawing_jinhyoung_2.cs
+++ Assets/Scripts/BlackBoard/Drawing_jinhyoung_2.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
+
 public class Drawing_jinhyoung_2 : MonoBehaviour
 {
+    [SerializeField]
     RawImage rawImage; // paint 전체
     RectTransform rt;
 
@@ -17,20 +19,23 @@ public class Drawing_jinhyoung_2 : MonoBehaviour
 
     Color drawColor; // 마우스 색갈
 
-    void Awake()
-    {
-        rawImage = GetComponent<RawImage>();
-        rt = rawImage.GetComponent<RectTransform>();
-        backGroundColor = blackboard.color;
-        drawColor = Color.white;
-    }
-
-    Texture2D paint; // int형만 넣을 수 있다. float형은 넣을 수 없다.
+    public static Texture2D paint; // int형만 넣을 수 있다. float형은 넣을 수 없다.
     int pixel_Width;  // 픽셀을 채울 크기
     int pixel_Height;
 
+    
+    Erase erase;
+    public GameObject erase2;
+
     void Start()
     {
+        rt = rawImage.GetComponent<RectTransform>();
+        backGroundColor = blackboard.color;
+        drawColor = Color.white;
+        erase = erase2.GetComponent<Erase>();
+        button_pen = GetComponent<Button>();
+        button_Erase = erase2.GetComponent<Button>();
+
         pixel_Width = (int)rt.rect.width;
         pixel_Height = (int)rt.rect.height;
 
@@ -52,41 +57,45 @@ public class Drawing_jinhyoung_2 : MonoBehaviour
 
     // 마우스의 이전 위치
     Vector2 lastPosition;
+
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (active)
         {
-            Vector2 currentPosition;
-
-            // 매개 변수로는 로컬 좌표로 변환하고 싶은 UI의 요소, 변환할 스크린 좌표, 변활을 수행할 때 사용할 카메라, 변환된 로컬 좌표가 반환되는 함수
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                rawImage.rectTransform, Input.mousePosition, null, out currentPosition); 
-
-            // print(currentPosition); // 피봇이 중심이다.
-
-            // 정확한 좌표 위치가 필요해서 아래 함수는 필요하다.
-            // 0.5f 더하기 전까지는 정규화가 필요하다. 
-            // 0.5f를 더해 중심을 기준으로 변환한 후, 이를 곱해 Texture2D의 픽셀 좌표로 변환한다.
-            Vector2 mousePosition = new Vector2
-            (
-                (currentPosition.x / rawImage.rectTransform.rect.width + 0.5f) * pixel_Width,
-                (currentPosition.y / rawImage.rectTransform.rect.height + 0.5f) * pixel_Height
-            );
-
-            if(lastPosition != null && lastPosition != Vector2.zero)
+            if (Input.GetMouseButton(0))
             {
-                Draw_Lerp(lastPosition, mousePosition);
+                Vector2 currentPosition;
+
+                // 매개 변수로는 로컬 좌표로 변환하고 싶은 UI의 요소, 변환할 스크린 좌표, 변활을 수행할 때 사용할 카메라, 변환된 로컬 좌표가 반환되는 함수
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                    rawImage.rectTransform, Input.mousePosition, null, out currentPosition);
+
+                // print(currentPosition); // 피봇이 중심이다.
+
+                // 정확한 좌표 위치가 필요해서 아래 함수는 필요하다.
+                // 0.5f 더하기 전까지는 정규화가 필요하다. 
+                // 0.5f를 더해 중심을 기준으로 변환한 후, 이를 곱해 Texture2D의 픽셀 좌표로 변환한다.
+                Vector2 mousePosition = new Vector2
+                (
+                    (currentPosition.x / rawImage.rectTransform.rect.width + 0.5f) * pixel_Width,
+                    (currentPosition.y / rawImage.rectTransform.rect.height + 0.5f) * pixel_Height
+                );
+
+                if (lastPosition != null && lastPosition != Vector2.zero)
+                {
+                    Draw_Lerp(lastPosition, mousePosition);
+                }
+                else
+                {
+                    Draw(mousePosition);
+                }
+                lastPosition = mousePosition;
+                paint.Apply();
             }
             else
             {
-                Draw(mousePosition);
+                lastPosition = Vector2.zero;
             }
-            lastPosition = mousePosition;
-            paint.Apply();
-        }
-        else
-        {
-            lastPosition = Vector2.zero;
         }
     }
     
@@ -111,24 +120,48 @@ public class Drawing_jinhyoung_2 : MonoBehaviour
 
     void Draw_Lerp(Vector2 lastPosition, Vector2 currentPosition)
     {
-        float  dist = Vector2.Distance(currentPosition, lastPosition);
+        float  dist = Vector2.Distance(currentPosition, lastPosition); 
 
         if(Mathf.RoundToInt(dist) == 0)
         {
             return;
         }
 
-        float dist_int = 1f / (float)Mathf.RoundToInt(dist);
+        float dist_int = 1f / (float)Mathf.RoundToInt(dist); // 1로 나누는 이유는 두 점 사이를 비율로 나누기 위해 (이거 한 번만 더 물어보기)
 
-        //Vector2 fillPosition = Vector2.Lerp(currentPosition,lastPosition, dist_int);
+        // 결론은, 1로 나누는 이유는 딱 그 두 점 사이를 100% 나누기 위해서인 거야. 쉽게 말해, 두 점 사이를 정확하게 나누고 싶을 때 1을 사용하는 거지.
 
-        for(int count = 0; count <= Mathf.RoundToInt(dist); count++)
+        for (int count = 0; count <= Mathf.RoundToInt(dist); count++)
         {
             Vector2 drowPoint = Vector2.Lerp(lastPosition , currentPosition, dist_int * count);
 
             Draw(drowPoint);
         }
+    }
 
-        // print(Mathf.RoundToInt(dist) + dist_int);
+   
+    // 마우스 활성화
+    public bool active = false;
+    public Button button_pen;
+    public void Button_Click()
+    {
+        active = true;
+        erase.active_erase = false;
+    }
+
+
+    public void Button_Change()
+    {
+        ColorBlock color = button_pen.colors;
+        color.normalColor = Color.green;
+        button_pen.colors = color;
+    }
+
+    Button button_Erase;
+    public void Button_Change_erase()
+    {
+        ColorBlock color = button_Erase.colors;
+        color.normalColor = Color.white;
+        button_Erase.colors = color;
     }
 }
--- Assets/Scripts/BlackBoard/Erase.cs
+++ Assets/Scripts/BlackBoard/Erase.cs
@@ -7,9 +7,9 @@ using UnityEngine.UI;
 
 public class Erase : MonoBehaviour
 {
-    RawImage background; // blackboard(지우개 색깔 구할려고)
-
     [SerializeField]
+    Image background; // blackboard(지우개 색깔 구할려고)
+
     Color erase_color; // 지우개 색깔
 
     [SerializeField]
@@ -17,66 +17,71 @@ public class Erase : MonoBehaviour
 
     RectTransform rt; // 그림판 크기
 
-    private void Awake()
-    {
-        background = GameObject.FindWithTag("blackboard").GetComponent<RawImage>();
-        rt = paint.GetComponent<RectTransform>();
-    }
-
-    private void Start()
-    {
-        erase_color = background.color;
-    }
-
     // 이전 좌표
     Vector2 lastPosition;
+
+    Drawing_jinhyoung_2 dj;
+    public GameObject pen;
+
+
+    public Button button;
+    private void Start()
+    {
+        rt = paint.GetComponent<RectTransform>();
+        dj = pen.GetComponent<Drawing_jinhyoung_2>();
+        button = GetComponent<Button>();
+        pen_Button = pen.GetComponent<Button>();
+    }
     void Update()
     {
-
-        if (Input.GetMouseButton(0))
+        // 지우개 버튼 눌렀을 때만 활성화
+        if (active_erase)
         {
-            // 현재 좌표
-            Vector2 currentPosition;
+            Erase_Button();
 
-            // 스크린 좌표를 화면 좌표로 바꾸기
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(rt, Input.mousePosition, null, out currentPosition); // 범위를 정해줘야 하니까
-
-            // currentPosition은 중점이 0.5, 0.5 이기 때문에
-            // 맨 왼쪽 아래 부터 (0, 0)으로 한 번 더 맞춰줘야 한다.
-            Vector2 mousePosition = new Vector2(
-            (currentPosition.x / rt.rect.width + 0.5f) * rt.rect.width,
-            (currentPosition.y / rt.rect.height + 0.5f) * rt.rect.height
-            );
-
-
-            if (lastPosition != null && lastPosition != Vector2.zero) // 전역 변수는 값이 없어도 사용 가능 but 지역 변수는 불가능
+            if (Input.GetMouseButton(0))
             {
-                Erase_Lerp(lastPosition, mousePosition);
+                // 현재 좌표
+                Vector2 currentPosition;
+
+                // 스크린 좌표를 화면 좌표로 바꾸기
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(rt, Input.mousePosition, null, out currentPosition); // 범위를 정해줘야 하니까
+
+                // currentPosition은 중점이 0.5, 0.5 이기 때문에
+                // 맨 왼쪽 아래 부터 (0, 0)으로 한 번 더 맞춰줘야 한다.
+                Vector2 mousePosition = new Vector2(
+                (currentPosition.x / rt.rect.width + 0.5f) * rt.rect.width,
+                (currentPosition.y / rt.rect.height + 0.5f) * rt.rect.height
+                );
+
+
+                if (lastPosition != null && lastPosition != Vector2.zero) // 전역 변수는 값이 없어도 사용 가능 but 지역 변수는 불가능
+                {
+                    Erase_Lerp(lastPosition, mousePosition);
+                }
+                else
+                {
+                    Erase_(mousePosition);
+                }
+                lastPosition = mousePosition;
+                pixel_Paint.Apply();
             }
             else
             {
-                Erase_(mousePosition);
+                lastPosition = Vector2.zero;
             }
-            lastPosition = mousePosition;
-            pixel_Paint.Apply();
-        }
-        else
-        {
-            lastPosition = Vector2.zero;
         }
     }
 
     Texture2D pixel_Paint;
     void Erase_(Vector2 mousePosition)
     {
-        pixel_Paint = paint.GetComponent<Texture2D>();
-
         int brush_Width = (int)mousePosition.x;
         int brush_Height = (int)mousePosition.y;
 
-        for(int height_Plus = -2; height_Plus <= 2; height_Plus++)
+        for(int height_Plus = -10; height_Plus <= 10; height_Plus++)
         {
-            for(int width_Plus = -2; width_Plus <= 2; width_Plus++)
+            for(int width_Plus = -10; width_Plus <= 10; width_Plus++)
             {
                 if(brush_Height + height_Plus >=0 && brush_Height + height_Plus < rt.rect.height &&
                     brush_Width + width_Plus >=0 && brush_Width + width_Plus < rt.rect.width)
@@ -89,6 +94,46 @@ public class Erase : MonoBehaviour
 
     void Erase_Lerp(Vector2 lastPosition, Vector2 currentPosition)
     {
-       
+        float distance = Vector2.Distance(currentPosition, lastPosition);
+
+        if(Mathf.RoundToInt(distance) == 0)
+        {
+            return;
+        }
+
+        float dist_int = 1.0f / (float)Mathf.RoundToInt(distance); // 1로 나누는 이유는 두 점 사이를 비율로 나누기 위해
+
+        for(int count = 0;  count <= Mathf.RoundToInt(distance); count++)
+        {
+            Vector2 erasePoint = Vector2.Lerp(lastPosition, currentPosition, dist_int * count);
+            Erase_(erasePoint);
+        }
+    }
+    public bool active_erase = false;
+    
+    public void Erase_Button()
+    {
+        erase_color = background.color;
+        pixel_Paint = Drawing_jinhyoung_2.paint;
+
+
+        active_erase = true;
+        dj.active = false;
+    }
+
+    public void Button_Change()
+    {
+        // 버튼이 활성화 되어있는 동안 색깔 바꾸는 법 => 원래 이렇게 귀찮나...?
+        ColorBlock color = button.colors;
+        color.normalColor = Color.green;
+        button.colors = color;
+    }
+
+    Button pen_Button;
+    public void Button_Change_pen()
+    {
+        ColorBlock color = pen_Button.colors;
+        color.normalColor = Color.white;
+        pen_Button.colors = color;
     }
 }
