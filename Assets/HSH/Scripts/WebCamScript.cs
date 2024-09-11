4f324d7fd7f5da4d2cb545555414c62c5f68b8e2
githeed <tmdgmldhk11@gmail.com>
2024-09-11 16:58:07 +0900

    webCam

---
+++ Assets/HSH/Scripts/WebCamScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCamScript : MonoBehaviour
+{
+    public RawImage webcamDisplay;
+    WebCamTexture webcamTexture;
+
+    void Start()
+    {
+        // 모든 웹캠 장치 목록을 가져옵니다.
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0)
+        {
+            Debug.Log("No webcam detected.");
+            return;
+        }
+
+        // 첫 번째 웹캠을 사용하여 WebCamTexture를 생성
+        webcamTexture = new WebCamTexture(devices[0].name);
+
+        // RawImage 컴포넌트에 웹캠 영상을 할당
+        webcamDisplay.texture = webcamTexture;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1) && webcamTexture.isPlaying == false)
+        {
+            webcamTexture.Play();
+            webcamDisplay.enabled = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && webcamTexture.isPlaying == true)
+        {
+            webcamTexture.Pause();
+            webcamDisplay.enabled = false;
+        }
+    }
+}
