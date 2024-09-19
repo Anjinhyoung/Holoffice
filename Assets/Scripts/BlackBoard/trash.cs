8371d92d233259e65aa99e2e5291159e4f933f29
wlsgud2075 <wlsgud2075@naver.com>
2024-09-19 16:39:13 +0900

    UI 화면 수정(더 좋게)

---
+++ Assets/Scripts/BlackBoard/trash.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class trash : MonoBehaviour
+{
+    Button trash_Button;
+
+    private void Awake()
+    {
+        trash_Button = GetComponent<Button>();
+    }
+
+    public void Button_On()
+    {
+
+    }
+
+    void Button_Off()
+    {
+
+    }
+}
